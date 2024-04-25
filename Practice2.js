01ab43c4b2cb1ec6e4d37d4ec8a411a2ab7b8b21
JoachimStein <steinjoachim19@gmail.com>
2024-04-25 14:42:51 +0600

    JS Array, Condition, Loop

---
+++ Practice2.js
@@ -0,0 +1,14 @@
+const names = [
+    "A",
+    "B",
+    "C"
+]
+
+for (let i = 0; i < names.length; i++) {
+    if (i == 0) {
+        console.log(names[i] + " for Apple");
+    }
+    else {
+        console.log(names[i])
+    }
+}
